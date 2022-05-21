e34480bf07e0a0a874a51d4740bb76e322528741
raju120405 <raju120405@gmail.com>
2022-05-22 00:39:02 +0600

    navbar and route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import About from "./Pages/About/About";
+import Home from "./Pages/Hone/Home";
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div >
-      <button class="btn btn-primary">Button</button>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
