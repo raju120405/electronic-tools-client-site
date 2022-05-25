37a0e959df919d21863f06779c5013d8e339ee96
raju120405 <raju120405@gmail.com>
2022-05-25 23:24:48 +0600

    login and token

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init'
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../Hooks/UseToken';
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
   const { register, formState: { errors }, handleSubmit } = useForm()
@@ -13,20 +14,29 @@ const Login = () => {
     loading,
     error,
   ] = useSignInWithEmailAndPassword(auth);
+  const [token] = useToken(user || gUser);
+
   let signInError;
   const navigate = useNavigate();
   const location = useLocation();
   let from = location.state?.from?.pathname || "/"
+
+  useEffect(() => {
+    if (token) {
+      navigate(from, { replace: true });
+    }
+  }, [token, from, navigate])
+
   if (loading || gLoading) {
     return <Loading></Loading>
   }
   if (error || gError) {
     signInError = <p className='text-red-500'>{error?.message || gError?.message}</p>
   }
-  if (gUser || user) {
-    navigate(from, { replace: true })
+  // if (gUser || user) {
+  //   navigate(from, { replace: true })
 
-  }
+  // }
 
   const onSubmit = data => {
     console.log(data);
